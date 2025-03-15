ae43371b95bc7b0f0747af9e68fdad2e42c52d49
yna2131 <jn7924@gmail.com>
2025-03-14 19:25:54 -0600

    Add and delete todo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { db } from "./db/db";
 import { todosTable } from "./db/schema";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -10,6 +13,23 @@ app.get("/", (req, res) => {
 
 app.get("/todos", async (req, res) => {
   const todos = await db.select().from(todosTable);
-  res.json(todos)
+  res.json(
+    todos.map((todo) => {
+      return { ...todo, status: Boolean(todo.status) };
+    })
+  );
 });
+
+app.post("/todos", async (req, res) => {
+  const data = z.object({ text: z.string().min(2) }).parse(req.body);
+  await db.insert(todosTable).values(data);
+  res.sendStatus(200);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = z.object({ id: z.coerce.number() }).parse(req.params);
+  await db.delete(todosTable).where(eq(todosTable.id, id));
+  res.sendStatus(200);
+});
+
 app.listen(3000, () => console.log("Listeining on port 3000..."));
